021784b8b95b0148a0c2a9546132c4c7885a0ccb
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 10:40:51 +0300

    Initial commit

---
+++ GDTask-6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(remove_glasnlde("Ты неудачник, лол)" ));
+
+static string remove_glasnlde(string input)
+{
+    List<char> glasnlde = new List<char>() { 'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е' };
+    string output = "";
+    foreach (var el in input)
+    {
+        if (!glasnlde.Contains(el))
+        {
+            output += el;
+        }
+    }
+
+    return output;
+}
